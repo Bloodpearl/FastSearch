5670a1e74457f2e3c61db286fd79e89eec810b05
JKL <jkolodziejczyk612@gmail.com>
2014-09-10 01:00:11 +0200

    Alfa 0.1 - base Application functions

---
+++ FastSearch/AppFunc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastSearch.Common
+{
+    public static class AppFunc
+    {
+        public static void SendError(string message)
+        {
+            SendError(new Exception(message));
+        }
+        public static void SendError(Exception exception)
+        {
+            if (AppSettings.blockOnError)
+            {
+                throw exception;
+            }
+            else
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        public static void Alert(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
